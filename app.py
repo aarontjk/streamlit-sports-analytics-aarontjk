44c6a5d945c5fa46c647a5e1910e714025e65c54
aaron teoh <aarontjk@hotmail.com>
2023-03-25 17:21:21 +0800

    edit

--- app.py
+++ app.py
@@ -30,9 +30,9 @@ with a3:
     away_team = st.selectbox("Away Team:", options = clubs)
 
 
-### Get players from each team
-home = ['test1', 'test2', 'test3', 'test4', 'test5', 'test6', 'test7', 'test8', 'test9', 'test10', 'test11']
-away = ['test1', 'test2', 'test3', 'test4', 'test5', 'test6', 'test7', 'test8', 'test9', 'test10', 'test11']
+### Get players from each team ###
+home_players = players.query(f"Club == '{home_team}'")
+away_players = players.query(f"Club == '{away_team}'")
 
 
 
@@ -42,32 +42,32 @@ c1, c2, c3 = st.columns(3)
 
 with c1:
     with st.form('Home Team Lineup:'):
-        home_player1 = st.selectbox("Enter Home Team Player 1:", options = home)
-        home_player2 = st.selectbox("Enter Home Team Player 2:", options = home)
-        home_player3 = st.selectbox("Enter Home Team Player 3:", options = home)
-        home_player4 = st.selectbox("Enter Home Team Player 4:", options = home)
-        home_player5 = st.selectbox("Enter Home Team Player 5:", options = home)
-        home_player6 = st.selectbox("Enter Home Team Player 6:", options = home)
-        home_player7 = st.selectbox("Enter Home Team Player 7:", options = home)
-        home_player8 = st.selectbox("Enter Home Team Player 8:", options = home)
-        home_player9 = st.selectbox("Enter Home Team Player 9:", options = home)
-        home_player10 = st.selectbox("Enter Home Team Player 10:", options = home)
-        home_player11 = st.selectbox("Enter Home Team Player 11:", options = home)
+        home_player1 = st.selectbox("Enter Home Team Player 1:", options = home_players)
+        home_player2 = st.selectbox("Enter Home Team Player 2:", options = home_players)
+        home_player3 = st.selectbox("Enter Home Team Player 3:", options = home_players)
+        home_player4 = st.selectbox("Enter Home Team Player 4:", options = home_players)
+        home_player5 = st.selectbox("Enter Home Team Player 5:", options = home_players)
+        home_player6 = st.selectbox("Enter Home Team Player 6:", options = home_players)
+        home_player7 = st.selectbox("Enter Home Team Player 7:", options = home_players)
+        home_player8 = st.selectbox("Enter Home Team Player 8:", options = home_players)
+        home_player9 = st.selectbox("Enter Home Team Player 9:", options = home_players)
+        home_player10 = st.selectbox("Enter Home Team Player 10:", options = home_players)
+        home_player11 = st.selectbox("Enter Home Team Player 11:", options = home_players)
         st.form_submit_button('Submit')
 
 
 
 with c3:
     with st.form('Away Team Lineup:'):
-        away_player1 = st.selectbox("Enter Away Team Player 1:", options = away)
-        away_player2 = st.selectbox("Enter Away Team Player 2:", options = away)
-        away_player3 = st.selectbox("Enter Away Team Player 3:", options = away)
-        away_player4 = st.selectbox("Enter Away Team Player 4:", options = away)
-        away_player5 = st.selectbox("Enter Away Team Player 5:", options = away)
-        away_player6 = st.selectbox("Enter Away Team Player 6:", options = away)
-        away_player7 = st.selectbox("Enter Away Team Player 7:", options = away)
-        away_player8 = st.selectbox("Enter Away Team Player 8:", options = away)
-        away_player9 = st.selectbox("Enter Away Team Player 9:", options = away)
-        away_player10 = st.selectbox("Enter Away Team Player 10:", options = away)
-        away_player11 = st.selectbox("Enter Away Team Player 11:", options = away)
+        away_player1 = st.selectbox("Enter Away Team Player 1:", options = away_players)
+        away_player2 = st.selectbox("Enter Away Team Player 2:", options = away_players)
+        away_player3 = st.selectbox("Enter Away Team Player 3:", options = away_players)
+        away_player4 = st.selectbox("Enter Away Team Player 4:", options = away_players)
+        away_player5 = st.selectbox("Enter Away Team Player 5:", options = away_players)
+        away_player6 = st.selectbox("Enter Away Team Player 6:", options = away_players)
+        away_player7 = st.selectbox("Enter Away Team Player 7:", options = away_players)
+        away_player8 = st.selectbox("Enter Away Team Player 8:", options = away_players)
+        away_player9 = st.selectbox("Enter Away Team Player 9:", options = away_players)
+        away_player10 = st.selectbox("Enter Away Team Player 10:", options = away_players)
+        away_player11 = st.selectbox("Enter Away Team Player 11:", options = away_players)
         st.form_submit_button('Submit')
