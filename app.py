b05c91aaf927e795e1eb2ae61167c7f872ed42e0
aaron teoh <aarontjk@hotmail.com>
2023-03-25 17:28:43 +0800

    edit

--- app.py
+++ app.py
@@ -32,10 +32,11 @@ with a3:
 
 ### Get players from each team ###
 home_players = players.query(f"Club == '{home_team}'")
-home_players = list(home_players['Name']).sort()
+home_players = list(home_players['Name'])
+home_players.sort()
 away_players = players.query(f"Club == '{away_team}'")
-away_players = list(away_players['Name']).sort()
-
+away_players = list(away_players['Name'])
+away_players.sort()
 
 
 ### Enter players ###
