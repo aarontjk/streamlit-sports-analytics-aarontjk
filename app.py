26eb71bd94d9bb5f6c0cd69313ed3b7d9469605c
aaron teoh <aarontjk@hotmail.com>
2023-03-25 15:07:13 +0800

    first push

---
+++ app.py
@@ -0,0 +1,16 @@
+import streamlit as st
+
+
+with st.form('Home Team Lineup:'):
+    home_player1 = st.selectbox("Enter Home Team Player 1:")
+    home_player2 = st.selectbox("Enter Home Team Player 2:")
+    home_player3 = st.selectbox("Enter Home Team Player 3:")
+    home_player4 = st.selectbox("Enter Home Team Player 4:")
+    home_player5 = st.selectbox("Enter Home Team Player 5:")
+    home_player6 = st.selectbox("Enter Home Team Player 6:")
+    home_player7 = st.selectbox("Enter Home Team Player 7:")
+    home_player8 = st.selectbox("Enter Home Team Player 8:")
+    home_player9 = st.selectbox("Enter Home Team Player 9:")
+    home_player10 = st.selectbox("Enter Home Team Player 10:")
+    home_player11 = st.selectbox("Enter Home Team Player 11:")
+    home_player12 = st.selectbox("Enter Home Team Player 12:")
