8f50b8e4124816c96f44e909a412e65197a9494f
aaron teoh <aarontjk@hotmail.com>
2023-03-25 16:41:20 +0800

    edit

--- app.py
+++ app.py
@@ -8,7 +8,8 @@ away = ['test1', 'test2', 'test3', 'test4', 'test5', 'test6', 'test7', 'test8', 
 
 ### UI Design Below ###
 
-st.markdown("<h1 style='text-align: center; color: red;'>SportsAnalytics \n Football Outcome Predictor</h1>", unsafe_allow_html=True)
+st.markdown("<h1 style='text-align: center; color: red;'>SportsAnalytics</h1>", unsafe_allow_html=True)
+st.markdown("<h1 style='text-align: center; color: red;'>Football Outcome Predictor</h1>", unsafe_allow_html=True)
 
 
 c1, c2, c3 = st.columns(3)
