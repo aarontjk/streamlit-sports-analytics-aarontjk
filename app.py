7d50ed170a089eda8c72083b260104e6820c75f8
aaron teoh <aarontjk@hotmail.com>
2023-04-06 21:06:17 +0800

    edit

--- app.py
+++ app.py
@@ -2,29 +2,7 @@ import streamlit as st
 import pandas as pd
 import requests
 
-""" def prep_data(home_lineup, home_form, away_lineup, away_form):
 
-    home_form_num = 0
-    for char in  home_form:
-        if char == "W":
-            home_form_num += 2
-        elif char == "D":
-            home_form_num += 1
-
-    away_form_num = 0
-    for char in  away_form:
-        if char == "W":
-            away_form_num += 2
-        elif char == "D":
-            away_form_num += 1
-
-
-    home_df = players[players['player_name'].isin(home_lineup)]
-
-    away_df = players[players['player_name'].isin(away_lineup)]
-
-    st.write(home_df)
-    st.write(away_df) """
 
 def check_submit(home_lineup, home_form, away_lineup, away_form):
     go = True
