c73cd137cd7ae9adc8bbf7271dd9bb87f3a1c110
aaron teoh <aarontjk@hotmail.com>
2023-04-08 12:02:26 +0800

    edit

--- app.py
+++ app.py
@@ -55,12 +55,12 @@ a1, a2, a3 = st.columns(3)
 
 with a1:
     home_team = st.selectbox("Home Team:", options = clubs)
-    home_team_id = clubs.query("home_team_name == '{home_team}'")['home_team_id']
+    home_team_id = clubs_df.query("home_team_name == '{home_team}'")['home_team_id']
 
 
 with a3:
     away_team = st.selectbox("Away Team:", options = clubs)
-    away_team_id = clubs.query("away_team_name == '{away_team}'")['away_team_id']
+    away_team_id = clubs_df.query("away_team_name == '{away_team}'")['away_team_id']
 
 
 ### Get players from each team ###
